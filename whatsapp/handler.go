504f3933eef22838f0eccd6acb25111aca8c8cbf
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2022-10-09 19:06:08 +0530

    Clean the whatsapp event handler

--- whatsapp/handler.go
+++ whatsapp/handler.go
@@ -12,59 +12,17 @@ import (
 
 	"github.com/PaulSonOfLars/gotgbot/v2"
 	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/proto"
 )
 
 func WhatsAppEventHandler(evt interface{}) {
-	waClient := state.State.WhatsAppClient
-	tgBot := state.State.TelegramBot
-	cfg := state.State.Config
-
 	switch v := evt.(type) {
 
-	case *events.TemporaryBan:
-		tgBot.SendMessage(
-			cfg.Telegram.TargetChatID,
-			fmt.Sprintf(
-				"#bans\n\nTemporarily Banned from WhatsApp: <code>%s</code>",
-				html.EscapeString(v.String()),
-			),
-			&gotgbot.SendMessageOpts{},
-		)
-
 	case *events.CallOffer:
-		// TODO: Check and handle group calls
-		var callerName string
-		caller, err := waClient.Store.Contacts.GetContact(v.CallCreator)
-		if err != nil || !caller.Found {
-			callerName = v.CallCreator.String()
-		} else {
-			callerName = caller.FullName
-			if callerName == "" {
-				callerName = caller.BusinessName
-			}
-			if callerName == "" {
-				callerName = caller.PushName
-			}
-			callerName += fmt.Sprintf(" [ %s ]", v.CallCreator.User)
-		}
-
-		tgBot.SendMessage(
-			cfg.Telegram.TargetChatID,
-			fmt.Sprintf(
-				`#calls
-
-You received a new call
-
-🧑: <i>%s</i>
-🕛: <code>%s</code>`,
-				html.EscapeString(callerName),
-				html.EscapeString(v.Timestamp.Local().Format(cfg.TimeFormat)),
-			),
-			&gotgbot.SendMessageOpts{},
-		)
+		NewCallOfferHandler(v)
 
 	case *events.Message:
 		text := ""
@@ -74,124 +32,172 @@ You received a new call
 			text = v.Message.GetConversation()
 		}
 
-		// Messages sent by me
 		if v.Info.IsFromMe {
-
-			// Get ID of the current chat
-			if text == ".id" {
-				_, err := waClient.SendMessage(context.Background(), v.Info.Chat, "",
-					&waProto.Message{
-						ExtendedTextMessage: &waProto.ExtendedTextMessage{
-							Text: proto.String(fmt.Sprintf(
-								"The ID of current chat is : ```%s```",
-								utils.MarkdownEscapeString(v.Info.Chat.String()),
-							)),
-							ContextInfo: &waProto.ContextInfo{
-								StanzaId:      proto.String(v.Info.ID),
-								Participant:   proto.String(v.Info.MessageSource.Sender.String()),
-								QuotedMessage: v.Message,
-							},
-						},
-					})
-				if err != nil {
-					log.Printf("[whatsapp] failed to reply to '.id' : %s", err)
-				}
-			}
-
-			// Tag everyone in the group
-			if v.Info.IsGroup &&
-				(strings.Contains(strings.ToLower(text), "@all") ||
-					strings.Contains(strings.ToLower(text), "@everyone")) {
-
-				groupInfo, err := waClient.GetGroupInfo(v.Info.Chat)
-				if err != nil {
-					log.Printf("[whatsapp] failed to get group info of '%s' : %s", v.Info.Chat.String(), err)
-				} else {
-
-					replyText := ""
-					mentionedParticipants := []string{}
-
-					for _, participant := range groupInfo.Participants {
-						if participant.JID.User == waClient.Store.ID.User {
-							continue
-						}
-						replyText += fmt.Sprintf("@%s ", participant.JID.User)
-						mentionedParticipants = append(mentionedParticipants, participant.JID.String())
-					}
-
-					_, err = waClient.SendMessage(context.Background(), v.Info.Chat, "",
-						&waProto.Message{
-							ExtendedTextMessage: &waProto.ExtendedTextMessage{
-								Text: proto.String(replyText),
-								ContextInfo: &waProto.ContextInfo{
-									StanzaId:      proto.String(v.Info.ID),
-									Participant:   proto.String(v.Info.MessageSource.Sender.String()),
-									QuotedMessage: v.Message,
-									MentionedJid:  mentionedParticipants,
-								},
-							},
-						},
-					)
-					if err != nil {
-						log.Printf("[whatsapp] failed to reply to '@all' : %s", err)
-					}
-				}
-			}
-
-			return
+			NewMessageFromMeHandler(text, v)
+		} else {
+			NewMessageFromOthersHandler(text, v)
 		}
 
-		if v.Info.IsGroup &&
-			slices.Contains(cfg.WhatsApp.TagAllAllowedGroups, v.Info.Chat.User) &&
-			(strings.Contains(strings.ToLower(text), "@all") ||
-				strings.Contains(strings.ToLower(text), "@everyone")) {
+	}
+}
 
-			groupInfo, err := waClient.GetGroupInfo(v.Info.Chat)
-			if err != nil {
-				log.Printf("[whatsapp] failed to get group info of '%s' : %s", v.Info.Chat.String(), err)
-			} else {
+func NewMessageFromMeHandler(text string, v *events.Message) {
+	// Get ID of the current chat
+	if text == ".id" {
+		GetChatIDHandler(v.Info.Chat, v.Message, v.Info.ID, v.Info.MessageSource.Sender.String())
+	}
 
-				replyText := ""
-				mentionedParticipants := []string{}
+	// Tag everyone in the group
+	if v.Info.IsGroup && (strings.Contains(strings.ToLower(text), "@all") || strings.Contains(strings.ToLower(text), "@everyone")) {
+		TagAllHandler(v.Info.Chat, v.Message, v.Info.ID, v.Info.MessageSource.Sender.String(), true)
+	}
+}
 
-				for _, participant := range groupInfo.Participants {
-					if participant.JID.User == waClient.Store.ID.User {
-						continue
-					}
-					replyText += fmt.Sprintf("@%s ", participant.JID.User)
-					mentionedParticipants = append(mentionedParticipants, participant.JID.String())
-				}
+func NewMessageFromOthersHandler(text string, v *events.Message) {
+	cfg := state.State.Config
 
-				_, err = waClient.SendMessage(context.Background(), v.Info.Chat, "",
-					&waProto.Message{
-						ExtendedTextMessage: &waProto.ExtendedTextMessage{
-							Text: proto.String(replyText),
-							ContextInfo: &waProto.ContextInfo{
-								StanzaId:      proto.String(v.Info.ID),
-								Participant:   proto.String(v.Info.MessageSource.Sender.String()),
-								QuotedMessage: v.Message,
-								MentionedJid:  mentionedParticipants,
-							},
-						},
-					},
-				)
-				if err != nil {
-					log.Printf("[whatsapp] failed to reply to '@all' : %s", err)
-				}
+	// Tag everyone in allowed groups
+	if v.Info.IsGroup && slices.Contains(cfg.WhatsApp.TagAllAllowedGroups, v.Info.Chat.User) &&
+		(strings.Contains(strings.ToLower(text), "@all") ||
+			strings.Contains(strings.ToLower(text), "@everyone")) {
+		TagAllHandler(v.Info.Chat, v.Message, v.Info.ID, v.Info.MessageSource.Sender.String(), false)
+	}
 
-				tgBot.SendMessage(
-					cfg.Telegram.TargetChatID,
-					fmt.Sprintf(
-						`#tagall
+	switch v.Info.MediaType {
+	case "image":
+
+	case "gif":
+
+	case "video":
+
+	case "ptt":
+		// Voice Notes
+
+	case "audio":
+
+	case "document":
+		// Any document like PDF, image, video etc
+
+	case "vcard":
+		// Contact
+
+	case "location":
+
+	default:
+
+	}
+}
+
+func GetChatIDHandler(chat types.JID, msg *waProto.Message, msgId string, msgSender string) {
+	waClient := state.State.WhatsAppClient
+
+	_, err := waClient.SendMessage(context.Background(), chat, "",
+		&waProto.Message{
+			ExtendedTextMessage: &waProto.ExtendedTextMessage{
+				Text: proto.String(fmt.Sprintf(
+					"The ID of current chat is : ```%s```",
+					utils.MarkdownEscapeString(chat.String()),
+				)),
+				ContextInfo: &waProto.ContextInfo{
+					StanzaId:      proto.String(msgId),
+					Participant:   proto.String(msgSender),
+					QuotedMessage: msg,
+				},
+			},
+		})
+	if err != nil {
+		log.Printf("[whatsapp] failed to reply to '.id' : %s\n", err)
+	}
+}
+
+func TagAllHandler(group types.JID, msg *waProto.Message, msgId string, msgSender string, msgIsFromMe bool) {
+	waClient := state.State.WhatsAppClient
+	tgBot := state.State.TelegramBot
+	cfg := state.State.Config
+
+	groupInfo, err := waClient.GetGroupInfo(group)
+	if err != nil {
+		log.Printf("[whatsapp] failed to get group info of '%s' : %s\n", group.String(), err)
+		return
+	}
+
+	replyText := ""
+	mentionedParticipants := []string{}
+
+	for _, participant := range groupInfo.Participants {
+		if participant.JID.User == waClient.Store.ID.User {
+			continue
+		}
+		replyText += fmt.Sprintf("@%s ", participant.JID.User)
+		mentionedParticipants = append(mentionedParticipants, participant.JID.String())
+	}
+
+	_, err = waClient.SendMessage(context.Background(), group, "",
+		&waProto.Message{
+			ExtendedTextMessage: &waProto.ExtendedTextMessage{
+				Text: proto.String(replyText),
+				ContextInfo: &waProto.ContextInfo{
+					StanzaId:      proto.String(msgId),
+					Participant:   proto.String(msgSender),
+					QuotedMessage: msg,
+					MentionedJid:  mentionedParticipants,
+				},
+			},
+		},
+	)
+	if err != nil {
+		log.Printf("[whatsapp] failed to reply to '@all' : %s\n", err)
+	}
+
+	if !msgIsFromMe {
+		tgBot.SendMessage(
+			cfg.Telegram.TargetChatID,
+			fmt.Sprintf(
+				`#tagall
 
 Everyone was mentioned in a group
 
 👥: <i>%s</i>`,
-						html.EscapeString(groupInfo.Name),
-					),
-					&gotgbot.SendMessageOpts{},
-				)
-			}
-		}
+				html.EscapeString(groupInfo.Name),
+			),
+			&gotgbot.SendMessageOpts{},
+		)
 	}
+}
+
+func NewCallOfferHandler(v *events.CallOffer) {
+	waClient := state.State.WhatsAppClient
+	tgBot := state.State.TelegramBot
+	cfg := state.State.Config
+
+	// TODO: Check and handle group calls
+	var callerName string
+	caller, err := waClient.Store.Contacts.GetContact(v.CallCreator)
+	if err != nil || !caller.Found {
+		callerName = v.CallCreator.String()
+	} else {
+		callerName = caller.FullName
+		if callerName == "" {
+			callerName = caller.BusinessName
+		}
+		if callerName == "" {
+			callerName = caller.PushName
+		}
+		callerName += fmt.Sprintf(" [ %s ]", v.CallCreator.User)
+	}
+
+	tgBot.SendMessage(
+		cfg.Telegram.TargetChatID,
+		fmt.Sprintf(
+			`#calls
+
+You received a new call
+
+🧑: <i>%s</i>
+🕛: <code>%s</code>`,
+			html.EscapeString(callerName),
+			html.EscapeString(v.Timestamp.Local().Format(cfg.TimeFormat)),
+		),
+		&gotgbot.SendMessageOpts{},
+	)
 }
