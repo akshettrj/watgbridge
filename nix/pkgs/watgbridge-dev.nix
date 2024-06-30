ffc973a54a660c6a5162ddc35c9bfa4e3f7adbe9
Akshett Rai Jindal <jindalakshett@gmail.com>
2024-06-30 17:20:29 +0530

    nix: remove unnecessary files from package source and disable action for aarch64-linux

--- nix/pkgs/watgbridge-dev.nix
+++ nix/pkgs/watgbridge-dev.nix
@@ -2,12 +2,33 @@
 , buildGoApplication
 }:
 
-buildGoApplication rec {
+let
+
+  localSrc = builtins.path {
+    path = ../../.;
+    name = "watgbridge";
+    filter = path: type: (
+      builtins.baseNameOf path != "flake.nix" &&
+      builtins.baseNameOf path != "flake.lock" &&
+      builtins.baseNameOf path != "README.md" &&
+      builtins.baseNameOf path != "sample_config.yaml" &&
+      builtins.baseNameOf path != "watgbridge.service.sample" &&
+      builtins.baseNameOf path != ".github" &&
+      builtins.baseNameOf path != "nix" &&
+      builtins.baseNameOf path != "assets" &&
+      builtins.baseNameOf path != ".envrc" &&
+      builtins.baseNameOf path != ".gitignore" &&
+      builtins.baseNameOf path != "Dockerfile" &&
+      builtins.baseNameOf path != "LICENSE"
+    );
+  };
+
+in buildGoApplication rec {
   pname = "watgbridge";
   version = "1.9.0";
 
-  pwd = ../../.;
-  src = ../../.;
+  pwd = localSrc;
+  src = localSrc;
 
   ldflags = [ "-s" "-w" ];
 
