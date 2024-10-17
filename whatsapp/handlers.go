e039bd2523acccdfb1a01de6872e7ee06317d074
Rian Felipe Celestrino <ribru29@gmail.com>
2024-10-17 12:38:05 -0300

    feat: making view_once messages become spoilers

--- whatsapp/handlers.go
+++ whatsapp/handlers.go
@@ -499,6 +499,7 @@ func MessageFromOthersEventHandler(text string, v *events.Message, isEdited bool
 				ReplyParameters: &gotgbot.ReplyParameters{
 					MessageId: replyToMsgId,
 				},
+				HasSpoiler:      *imageMsg.ViewOnce,
 				MessageThreadId: threadId,
 			})
 			if sentMsg.MessageId != 0 {
